55b09c986dabc107581b9fabcdc97d50d2943cee
pedias <pedias@microsoft.com>
2018-01-15 12:51:17 +0100

    Implement Dummy Logger (logging to Diagnostics.Debug.Console)

---
+++ OfficeGraphTest/Logger.cs
@@ -0,0 +1,17 @@
+﻿using OfficeGraphTest.Domain.Contracts;
+using System;
+using System.Diagnostics;
+
+namespace OfficeGraphTest
+{
+    /// <summary>
+    /// Dummy implementation of the ILogger interface
+    /// </summary>
+    public class Logger : ILogger
+    {
+        public void LogException(Exception exception)
+        {
+            Debug.WriteLine($"EXCEPTION>> {exception.Message} <<<");
+        }
+    }
+}
