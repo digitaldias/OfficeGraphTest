daaf8d241f2f175eb718a6e40d44e5940249263d
pedias <pedias@microsoft.com>
2018-01-15 13:03:22 +0100

    Prepare main program for Asynchronous execution

--- OfficeGraphTest/Program.cs
+++ OfficeGraphTest/Program.cs
@@ -1,16 +1,42 @@
-﻿namespace OfficeGraphTest
+﻿using OfficeGraphTest.DependencyInversion;
+using OfficeGraphTest.Domain.Contracts;
+using StructureMap;
+using System.Threading.Tasks;
+
+namespace OfficeGraphTest
 {
     class Program
     {
+        // ensure that IOC is set up and configured 
+        private static Container _container = new Container(new RuntimeRegistry());
+
         static void Main(string[] args)
         {
-            // First, we'll need to authenticate ourselves against a trusted identity provider
+            RunExamplesAsync().Wait();
+
+            System.Console.WriteLine("Program completed");
+        }
 
 
-            // Next, well need to connect to the office graph api, and pass it our identity token
+        public static async Task RunExamplesAsync()
+        {
+            IOfficeGraphClient officeGraphClient = CreateAndInializeOfficeGraphClient();
 
             // and then, we can start to explore the fun stuff!!
+            var me = officeGraphClient.GetMyInformationAsync();
+        }
 
+
+        private static IOfficeGraphClient CreateAndInializeOfficeGraphClient()
+        {
+            // First, we'll need to authenticate ourselves against a trusted identity provider
+            var identityManager = _container.GetInstance<IIdentityManager>();
+
+            // Next, well need to connect to the office graph api, and pass it our identity token
+            var officeGraphClient = _container.GetInstance<IOfficeGraphClient>();
+
+            officeGraphClient.Initialize(identityManager);
+            return officeGraphClient;
         }
     }
 }
