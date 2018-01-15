2b6d1c22ab65e9a335abed3bc157b46c8e764930
pedias <pedias@microsoft.com>
2018-01-15 13:03:47 +0100

    Introduce IdentityManager and OfficeGraphClient interfaces. Also define Graph user from example JSON

---
+++ OfficeGraphTest.Domain/Contracts/IOfficeGraphClient.cs
@@ -0,0 +1,12 @@
+﻿using OfficeGraphTest.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace OfficeGraphTest.Domain.Contracts
+{
+    public interface IOfficeGraphClient
+    {
+        void Initialize(IIdentityManager identityManager);
+
+        Task<GraphUser> GetMyInformationAsync();
+    }
+}
---
+++ OfficeGraphTest.Domain/Entities/GraphUser.cs
@@ -0,0 +1,30 @@
+﻿namespace OfficeGraphTest.Domain.Entities
+{
+    public class GraphUser
+    {
+        public string odatacontext { get; set; }
+
+        public string id { get; set; }
+
+        public string[] businessPhones { get; set; }
+
+        public string displayName { get; set; }
+
+        public string givenName { get; set; }
+
+        public string jobTitle { get; set; }
+
+        public string mail { get; set; }
+
+        public object mobilePhone { get; set; }
+
+        public string officeLocation { get; set; }
+
+        public string preferredLanguage { get; set; }
+
+        public string surname { get; set; }
+
+        public string userPrincipalName { get; set; }
+    }
+
+}
